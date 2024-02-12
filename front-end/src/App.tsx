e1344ea6c4a84d8e69d8aa6e76bf056659fc32b8
Be4nz <benas.andriuscenka@gmail.com>
2024-02-12 17:40:13 +0200

    first commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src={
+          "https://media.licdn.com/dms/image/D4D03AQF7Exzm1xLBGg/profile-displayphoto-shrink_800_800/0/1694100310637?e=1713398400&v=beta&t=cRR7uJ1fSi7rTHWrr3MzcoAu1hAHV-nSX0B6OT13IrQ"
+        }
+        alt="aleksas"
+      />
+      <img
+        src={
+          "https://media.licdn.com/dms/image/D4D03AQEsk3mXJxMa8A/profile-displayphoto-shrink_800_800/0/1702309556141?e=1713398400&v=beta&t=3BbCZJayWVBa8UzvFutbXABj7HlLPFiJ876kVaNv5qU"
+        }
+        alt="justinas"
+      />
+      <img
+        src={
+          "https://scontent.fkun1-1.fna.fbcdn.net/v/t39.30808-6/376488448_3062649247202145_550087251620875475_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LAy22io8D4QAX9epXd9&_nc_ht=scontent.fkun1-1.fna&oh=00_AfCzXWTwAFrSrzPsJGzrXN4JSSjq6pd_NJDB7_lckePmqQ&oe=65CF629C"
+        }
+        alt="mante"
+      />
+    </div>
+  );
+}
+
+export default App;
