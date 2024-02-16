e06d8a3eb27d743a4e3f9af757148599a6c6f807
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-16 20:53:23 +0200

    SENT-23 wrapped application into Auth0Provider for authorization implementation

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ''}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ''}
+      authorizationParams={{ 
+        //audience: process.env.REACT_APP_AUTH0_AUDIENCE,       // might need to add this or change it
+        redirect_uri: window.location.origin,
+      }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
