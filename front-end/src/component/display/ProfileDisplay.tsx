7184c22acdccd055dd895f08c9a9457feb894329
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-16 20:56:25 +0200

    SENT-23 implemented profile display component for testing with account authorization

---
+++ front-end/src/component/display/ProfileDisplay.tsx
@@ -0,0 +1,24 @@
+import { Typography, Box, Avatar } from "@mui/material";
+import { useUserContext } from "../../context/UserContext";
+
+const Profile = () => {
+    const UserContext = useUserContext();
+
+    return (
+        <div>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="h2">Profile</Typography>
+                <Avatar src={UserContext.picture} alt="Profile" sx={{ width: 200, height: 200, my: 2 }} />
+                <Typography>Id: {UserContext.id}</Typography>
+                <Typography>Username: {UserContext.username}</Typography>
+                <Typography>Name: {UserContext.name}</Typography>
+                <Typography>Email: {UserContext.email}</Typography>
+                <Box sx={{ width: 300, height: 200, my: 2 }}>
+                    <Typography sx={{ overflow: 'auto', whiteSpace: 'pre' }}>Token: {UserContext.token}</Typography>
+                </Box>
+            </Box>
+        </div>
+    );
+};
+
+export default Profile;
