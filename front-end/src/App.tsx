d0820ff446c94688a0c5dc5ca40b314a83812087
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-16 20:58:34 +0200

    SENT-23 implemented routing and authorization into application

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,27 +1,32 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserContextProvider } from "./context/UserContext";
+import { AppRoute } from "./type/AppRoute";
+import Home from "./page/Home";
+import { CircularProgress } from "@mui/material";
+import Signup from "./page/Signup";
 
-function App() {
+const App = () => {
+  const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
+  if (!isAuthenticated) {
+    loginWithRedirect();
+    return <CircularProgress />;
+  }
+
   return (
-    <div className="App">
-      <img
-        src={
-          "https://media.licdn.com/dms/image/D4D03AQF7Exzm1xLBGg/profile-displayphoto-shrink_800_800/0/1694100310637?e=1713398400&v=beta&t=cRR7uJ1fSi7rTHWrr3MzcoAu1hAHV-nSX0B6OT13IrQ"
-        }
-        alt="aleksas"
-      />
-      <img
-        src={
-          "https://media.licdn.com/dms/image/D4D03AQEsk3mXJxMa8A/profile-displayphoto-shrink_800_800/0/1702309556141?e=1713398400&v=beta&t=3BbCZJayWVBa8UzvFutbXABj7HlLPFiJ876kVaNv5qU"
-        }
-        alt="justinas"
-      />
-      <img
-        src={
-          "https://scontent.fkun1-1.fna.fbcdn.net/v/t39.30808-6/376488448_3062649247202145_550087251620875475_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=LAy22io8D4QAX9epXd9&_nc_ht=scontent.fkun1-1.fna&oh=00_AfCzXWTwAFrSrzPsJGzrXN4JSSjq6pd_NJDB7_lckePmqQ&oe=65CF629C"
-        }
-        alt="mante"
-      />
-    </div>
+    <UserContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.HOME} element={<Home />} />
+          <Route path={AppRoute.SIGNUP} element={<Signup/>} />
+        </Routes>
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
 
