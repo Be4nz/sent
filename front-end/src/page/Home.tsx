d4a9fe8fdd7746a5286f8790ff190c738c2cbc70
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-16 20:57:32 +0200

    SENT-24 created page view and routing enumerator

---
+++ front-end/src/page/Home.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import ProfileDisplay from "../component/display/ProfileDisplay";
+
+const Home = () => {
+    return (
+        <Grid>
+            <ProfileDisplay />
+        </Grid>
+    );
+}
+
+export default Home;
