bf70d726b8a69a0aa6bcc2ad03e0cf42ff3f542a
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-21 16:12:45 +0200

    SENT-29 setting up .env variables

--- back-end/src/database/knexfile.ts
+++ back-end/src/database/knexfile.ts
@@ -1,14 +1,15 @@
 import type { Knex } from "knex";
+require("dotenv").config(); // Not working...
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql",
     connection: {
-      host: "localhost",
-      port: 3307,
-      database: "sent",
-      user: "username",
-      password: "password"
+      host: process.env.DB_HOST || "localhost",
+      port: Number(process.env.DB_PORT) || 3307,
+      database: process.env.DB_DATABASE || "sent",
+      user: process.env.DB_USER || "username",
+      password: process.env.DB_PASSWORD || "password"
     },
     migrations: {
       tableName: "knex_migrations",
