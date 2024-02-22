4cc26d27b8ab990f6f7acd1c499650f9a6c3e684
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-02-23 01:41:12 +0200

    SENT-29 migrations update

--- back-end/src/database/migrations/20240221120548_init.ts
+++ back-end/src/database/migrations/20240221120548_init.ts
@@ -1,23 +1,23 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.dropTableIfExists("user");
+    await knex.schema.dropTableIfExists("users");
 
-    await knex.schema.createTable("user", (table) => {
-        table.uuid("id").defaultTo(knex.raw('(UUID())')).primary();
+    await knex.schema.createTable("users", (table) => {
+        table.string('id').primary().defaultTo(knex.raw('(UUID())'));
         table.string('auth0_id').notNullable().unique();
         table.string('username').notNullable().unique();
         table.string('name').notNullable();
         table.string('email').notNullable().unique();
         table.string('role').defaultTo('user');
         table.string('picture').defaultTo('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsbcf.fr%2Fwp-content%2Fuploads%2F2018%2F03%2Fsbcf-default-avatar.png&f=1&nofb=1&ipt=041c528388efe43c20b3872742aa897b5a0111dae54f8b7368835f7aa066dfd8&ipo=images');
-        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.datetime('created_at').defaultTo(knex.fn.now());
         table.integer('followers').defaultTo(0);
         table.integer('following').defaultTo(0);
     });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTableIfExists("user");
+    await knex.schema.dropTableIfExists("users");
 }
 
--- back-end/src/database/seeds/init.ts
+++ back-end/src/database/seeds/init.ts
@@ -1,8 +1,8 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
-    await knex("user").truncate();
-    await knex("user").insert([
+    await knex("users").truncate();
+    await knex("users").insert([
         { 
             id: knex.raw("DEFAULT"),
             auth0_id: "auth0|65d2833c7728d88f53ac1b41",
@@ -17,7 +17,7 @@ export async function seed(knex: Knex): Promise<void> {
         },
         { 
             id: knex.raw("DEFAULT"),
-            auth0_id: "auth0|auth0|65cf987f3d77dce50c006f4c",
+            auth0_id: "auth0|65cf987f3d77dce50c006f4c",
             username: "user",
             name: "user@gmail.com",
             email: "user@gmail.com",
