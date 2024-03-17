8e6ac8004f27911a6e0f9ca71623f8a201d40b8b
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-03-17 20:57:42 +0200

    SENT-33 removed commented code and minor change in back-end

--- back-end/src/APIs/repositories/userRepository.ts
+++ back-end/src/APIs/repositories/userRepository.ts
@@ -1,41 +1,30 @@
-import knexConnection from "../../database/knex";
-import { UserModel } from "../../models";
+import knexConnection from '../../database/knex';
+import { UserModel } from '../../models';
 
 export const createUserRepository = async (user: UserModel) => {
-    const response = await knexConnection("users")
-    .insert(user);
-    return response;
+	const response = await knexConnection('users').insert(user);
+	return response;
 };
 
 export const readUserAuth0Repository = async (auth0_id: string) => {
-    const response = await knexConnection("users")
-    .select("auth0_id")
-    .where("auth0_id", auth0_id)
-    .limit(1);
-    return response[0] as UserModel;
+	const response = await knexConnection('users').select().where('auth0_id', auth0_id).limit(1);
+	return response[0] as UserModel;
 };
 
 export const readUserRepository = async (id: string) => {
-    const response = await knexConnection("users")
-    .select()
-    .where("id", id);
-    return response[0] as UserModel;
+	const response = await knexConnection('users').select().where('id', id);
+	return response[0] as UserModel;
 };
 
 export const readUsersRepository = async () => {
-    const response = await knexConnection("users")
-    .select();
-    return response as UserModel[];
+	const response = await knexConnection('users').select();
+	return response as UserModel[];
 };
 
 export const updateUserRepository = async (id: string, user: UserModel) => {
-    await knexConnection("users")
-    .update(user)
-    .where("id", id);
+	await knexConnection('users').update(user).where('id', id);
 };
 
 export const deleteUserRepository = async (id: string) => {
-    await knexConnection("users")
-    .where("id", id)
-    .del();
-}
+	await knexConnection('users').where('id', id).del();
+};
--- front-end/src/component/display/ProfileDisplay.tsx
+++ front-end/src/component/display/ProfileDisplay.tsx
@@ -114,40 +114,6 @@ const ProfileDisplay: React.FC<Props> = (props) => {
 			</Grid>
 		</Grid>
 	);
-
-	// return (
-	// 	<>
-	// 		{!User.isLoading && (
-	// 			<div>
-	// 				<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-	// 					<Typography variant='h2'>Profile</Typography>
-	// 					<Avatar src={User.picture} alt='Profile' sx={{ width: 200, height: 200, my: 2 }} />
-	// 					<Typography>Id: {User.id}</Typography>
-	// 					<Typography>Auth0: {User.auth0_id}</Typography>
-	// 					<Typography>Username: {User.username}</Typography>
-	// 					<Typography>Name: {User.name}</Typography>
-	// 					<Typography>Email: {User.email}</Typography>
-	// 					<Typography>Description: {User.description}</Typography>
-	// 					<Typography>Role: {User.role}</Typography>
-	// 					<Typography>Created at: {User.created_at.toString()}</Typography>
-	// 					<Typography>Followers: {User.followers}</Typography>
-	// 					<Typography>Following: {User.following}</Typography>
-	// 					<Typography>IsNewUser: {User.isNewUser.toString()}</Typography>
-	// 					<Box sx={{ width: 300, height: 200, my: 2 }}>
-	// 						<Typography sx={{ overflow: 'auto', whiteSpace: 'pre' }}>Token: {User.token}</Typography>
-	// 					</Box>
-	// 					<Button
-	// 						onClick={() => {
-	// 							update();
-	// 						}}
-	// 					>
-	// 						Change color mode
-	// 					</Button>
-	// 				</Box>
-	// 			</div>
-	// 		)}
-	// 	</>
-	// );
 };
 
 export default ProfileDisplay;
