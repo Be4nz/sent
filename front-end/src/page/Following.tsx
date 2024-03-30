07b4c0323e06068633cf0ffc9bbec3e402375215
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-03-30 02:13:14 +0200

    SENT-76 removed post form display

--- front-end/src/page/Following.tsx
+++ front-end/src/page/Following.tsx
@@ -2,9 +2,7 @@ import { useUserContext } from '../context/UserContext';
 import { useEffect, useState } from 'react';
 import { PostModel } from '../model';
 import LoadingDisplay from '../component/display/LoadingDisplay';
-import { Grid, useTheme } from '@mui/material';
-import PostForm from '../component/form/PostForm';
-import PostModal from '../component/display/PostModal';
+import { Grid } from '@mui/material';
 import PostListDisplay from '../component/display/PostListDisplay';
 import { get } from '../api/Api';
 
@@ -13,18 +11,8 @@ interface Props {}
 const Home: React.FC<Props> = (props) => {
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [posts, setPosts] = useState<PostModel[]>([]);
-	const [modalOpen, setModalOpen] = useState<boolean>(false);
 
 	const User = useUserContext();
-	const Theme = useTheme();
-
-	const handleModalOpen = () => {
-		setModalOpen(true);
-	};
-
-	const handleModalClose = () => {
-		setModalOpen(false);
-	};
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -48,22 +36,6 @@ const Home: React.FC<Props> = (props) => {
 	return (
 		<div style={{ width: '100%' }}>
 			<Grid item minWidth='360px' maxWidth='752px' mx='auto'>
-				<Grid
-					onClick={handleModalOpen}
-					my='4vh'
-					sx={{
-						borderRadius: '15px',
-						':hover': {
-							cursor: 'pointer',
-							backgroundColor: Theme.palette.background.paper,
-						},
-					}}
-				>
-					<PostForm disabled={true} />
-				</Grid>
-
-				<PostModal open={modalOpen} handleClose={handleModalClose} />
-
 				<PostListDisplay posts={posts} />
 			</Grid>
 		</div>
