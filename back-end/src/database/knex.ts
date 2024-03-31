0ff2b874c8655d9ea9605e320e08b26967991ece
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-03-31 22:30:39 +0300

    SENT-72 knex-paginate

--- back-end/src/database/knex.ts
+++ back-end/src/database/knex.ts
@@ -1,7 +1,8 @@
-const knex = require("knex");
-const knexfile = require("./knexfile");
-
+const knex = require('knex');
+const knexfile = require('./knexfile');
+const { attachPaginate } = require('knex-paginate');
 const knexConnection = knex(knexfile.development);
+attachPaginate();
 
 module.exports = knexConnection;
 
