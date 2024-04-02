42daa383705834b19620ace7457c633085ba8ccf
Justinas <156369263+justinnas@users.noreply.github.com>
2024-04-02 04:36:49 +0300

    Added an icon displayed for users with no posts

--- front-end/src/component/display/PostListDisplay.tsx
+++ front-end/src/component/display/PostListDisplay.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { PostModel } from '../../model';
 import PostDisplay from './PostDisplay';
-import { List, Typography } from '@mui/material';
+import { Box, List, Typography, useTheme } from '@mui/material';
+import CancelScheduleSendIcon from '@mui/icons-material/CancelScheduleSend';
 
 interface Props {
 	posts: PostModel[];
 }
 
 const PostListDisplay: React.FC<Props> = (props) => {
+	const Theme = useTheme();
+
 	if (props.posts.length === 0) {
-		return <Typography textAlign={'center'}>No posts found</Typography>;
+		return (
+			<Box style={{ textAlign: 'center', paddingTop: '5em' }}>
+				<CancelScheduleSendIcon
+					sx={{
+						fontSize: { xs: '50px', lg: '70px' },
+						color: Theme.palette.text.secondary,
+					}}
+				/>
+				<Typography style={{ textAlign: 'center', color: Theme.palette.text.secondary }}>No posts sent yet</Typography>
+			</Box>
+		);
 	}
 
 	return (
