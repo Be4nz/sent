6f686fcceb514f69e82585ea09b8a0e97bf45139
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-04-10 04:09:08 +0300

    implemented follow associated tests

--- front-end/src/tests/E2E-tests/02_followTests.spec.ts
+++ front-end/src/tests/E2E-tests/02_followTests.spec.ts
@@ -1,13 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { ProfilePage } from '../pages/profilePage';
 
-test('Follow user functionality works correctly', async ({ page }) => {
+const user = 'admin';
+
+const setupProfilePage = async (page: Page, username: string) => {
 	const profilePage = new ProfilePage(page);
-
-	const user = 'user';
-
 	// Go to the profile page of the user we want to follow
-	await profilePage.gotoProfile(user);
+	await profilePage.gotoProfile(username);
+	await profilePage.check();
+	return profilePage;
+};
+
+test('Follow user functionality works correctly', async ({ page }) => {
+	const profilePage = await setupProfilePage(page, user);
 
 	// Check initial state: Follow button should be present and contain "Follow"
 	await expect(profilePage.followButton).toHaveText('Follow');
@@ -23,4 +28,45 @@ test('Follow user functionality works correctly', async ({ page }) => {
 
 	// Check if the unfollow action was successful: Follow button should now contain "Follow" again
 	await expect(profilePage.followButton).toHaveText('Follow');
+});
+
+test('Followers display button works correctly', async ({ page }) => {
+	const profilePage = await setupProfilePage(page, user);
+
+	// Click on the followers button
+	await profilePage.followersButton.click();
+
+	const modal = await profilePage.getFollowersModal();
+
+	await expect(modal).toBeVisible();
+});
+
+test('Following display button works correctly', async ({ page }) => {
+	const profilePage = await setupProfilePage(page, user);
+
+	// Click on the following button
+	await profilePage.followingButton.click();
+
+	const modal = await profilePage.getFollowingModal();
+
+	await expect(modal).toBeVisible();
+});
+
+test('Followers and following modal close button works correctly', async ({ page }) => {
+	const profilePage = await setupProfilePage(page, user);
+
+	// Click on the following button
+	await profilePage.followingButton.click();
+
+	const modal = await profilePage.getFollowingModal();
+	const button = await profilePage.getFollowModalCloseButton();
+
+	await expect(modal).toBeVisible();
+	await expect(button).toBeVisible();
+
+	// Click on the close button
+	await button.click();
+
+	await expect(modal).not.toBeVisible();
+	await expect(button).not.toBeVisible();
 });
