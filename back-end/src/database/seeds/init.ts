4c31e62b744d5b2e12c17f54a1d46a7e1272332d
Mantvydas Deltuva <mantvydasdeltuva@gmail.com>
2024-04-10 04:06:21 +0300

    configured for registered and unregistered testing

--- back-end/src/database/seeds/init.ts
+++ back-end/src/database/seeds/init.ts
@@ -1,7 +1,13 @@
 import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
+	await knex('comments').del();
+	await knex.raw(`ALTER TABLE comments AUTO_INCREMENT = 1`);
+
 	await knex('follows').del();
+
 	await knex('posts').del();
+	await knex.raw(`ALTER TABLE posts AUTO_INCREMENT = 1`);
+
 	await knex('users').del();
 }
